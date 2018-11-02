3f073e6d9b2849ea6d48f46165c197156b3894bb
Kafakk <dreamwstt@gmail.com>
2018-11-02 18:17:50 +0700

    initial commit

---
+++ xdr_builder.go
@@ -0,0 +1,246 @@
+package xdrbuilder
+
+import (
+	"fmt"
+
+	"github.com/stellar/go/price"
+	"github.com/stellar/go/xdr"
+)
+
+type Asset struct {
+	XDRAsset xdr.Asset
+}
+
+type Path struct {
+	XDRAsset []xdr.Asset //max size is 5
+}
+
+func SetAsset(code, issuerPublicKey string) (respAsset Asset, err error) {
+	var issuer xdr.AccountId
+	err = issuer.SetAddress(issuerPublicKey)
+	if err != nil {
+		panic(err)
+	}
+	// var asset AssetHelp
+
+	err = respAsset.XDRAsset.SetCredit(code, issuer)
+
+	return respAsset, err
+}
+
+func SetNativeAsset() (respAsset Asset, err error) {
+	err = respAsset.XDRAsset.SetNative()
+	return respAsset, err
+}
+
+func CreateAccount(destinationPublicKey string, startingBalance int) (respXDR string, err error) {
+	var destination xdr.AccountId
+	err = destination.SetAddress(destinationPublicKey)
+	if err != nil {
+		panic(err)
+	}
+	op := xdr.CreateAccountOp{
+		Destination:     destination,
+		StartingBalance: xdr.Int64(startingBalance) * 10000000,
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+
+	return respXDR, err
+
+}
+
+func Payment(destinationPublicKey string, asset Asset, amount int) (respXDR string, err error) {
+	var destination xdr.AccountId
+	err = destination.SetAddress(destinationPublicKey)
+	if err != nil {
+		panic(err)
+	}
+
+	op := xdr.PaymentOp{
+		Destination: destination,
+		Asset:       asset.XDRAsset,
+		Amount:      xdr.Int64(amount) * 10000000,
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+
+	return respXDR, err
+
+}
+
+func ManageOffer(selling Asset, buying Asset, amount int, priceString string) (respXDR string, err error) {
+
+	price, err := price.Parse(priceString)
+	if err != nil {
+		panic(err)
+	}
+	op := xdr.ManageOfferOp{
+		Selling: selling.XDRAsset,
+		Buying:  buying.XDRAsset,
+		Amount:  xdr.Int64(amount) * 10000000,
+		Price:   price,
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+	return respXDR, err
+}
+
+func CreatePassiveOffer(selling Asset, buying Asset, amount int, priceString string) (respXDR string, err error) {
+
+	price, err := price.Parse(priceString)
+	if err != nil {
+		panic(err)
+	}
+	op := xdr.CreatePassiveOfferOp{
+		Selling: selling.XDRAsset,
+		Buying:  buying.XDRAsset,
+		Amount:  xdr.Int64(amount) * 10000000,
+		Price:   price,
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+
+	return respXDR, err
+}
+
+func AllowTrust(trustorPublicKey string, asset Asset, authorize bool) (respXDR string, err error) {
+
+	// asset.Extract(asset.Type)
+
+	// var assetType xdr.AllowTrustOpAsset
+	var allowTrustAsset xdr.AllowTrustOpAsset
+
+	assetType := asset.XDRAsset.Type
+
+	fmt.Println(assetType)
+
+	switch assetType {
+	case 0:
+		return "This is Native Asset", nil
+	case 1:
+		code, ok := asset.XDRAsset.GetAlphaNum4()
+		if ok == false {
+			panic("The value is not valid")
+		}
+		allowTrustAsset = xdr.AllowTrustOpAsset{
+			Type:       1,
+			AssetCode4: &code.AssetCode,
+		}
+	case 2:
+		code, ok := asset.XDRAsset.GetAlphaNum12()
+		if ok == false {
+			panic("The value is not valid")
+		}
+		allowTrustAsset = xdr.AllowTrustOpAsset{
+			Type:        1,
+			AssetCode12: &code.AssetCode,
+		}
+	}
+
+	var trustor xdr.AccountId
+	err = trustor.SetAddress(trustorPublicKey)
+	if err != nil {
+		panic(err)
+	}
+
+	op := xdr.AllowTrustOp{
+		Trustor:   trustor,
+		Asset:     allowTrustAsset,
+		Authorize: authorize,
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+	return respXDR, err
+}
+
+func PathPayment(sendAsset Asset, sendMax int, destinationPublicKey string, destAsset Asset, destAmount int, path Path) (respXDR string, err error) {
+
+	var destination xdr.AccountId
+	err = destination.SetAddress(destinationPublicKey)
+	if err != nil {
+		panic(err)
+	}
+	op := xdr.PathPaymentOp{
+		SendAsset:   sendAsset.XDRAsset,
+		SendMax:     xdr.Int64(sendMax) * 10000000,
+		Destination: destination,
+		DestAsset:   destAsset.XDRAsset,
+		DestAmount:  xdr.Int64(destAmount) * 10000000,
+		Path:        path.XDRAsset,
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+
+	return respXDR, err
+}
+
+func ChangeTrust(asset Asset, limit int) (respXDR string, err error) {
+	op := xdr.ChangeTrustOp{
+		Line:  asset.XDRAsset,
+		Limit: xdr.Int64(limit),
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+
+	return respXDR, err
+}
+
+func BumpSequence(sequenceNumber int) (respXDR string, err error) {
+	op := xdr.BumpSequenceOp{
+		BumpTo: xdr.SequenceNumber(sequenceNumber),
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+
+	return respXDR, err
+}
+
+func ManageData(name, value string) (respXDR string, err error) {
+	dataname := xdr.String64(name)
+
+	valueByte := []byte(value)
+	datavalue := xdr.DataValue(valueByte)
+
+	op := xdr.ManageDataOp{
+		DataName:  dataname,
+		DataValue: &datavalue,
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+
+	return respXDR, err
+}
+
+func SetOption(inflationDestinationPublicKey string, clearFlag, setFlag, masterWeight,
+	lowThreshold, medThreshold, highThreshold uint32, homeDomain string, singerPublicKey string, singerWeight uint32) (respXDR string, err error) {
+
+	var destination xdr.AccountId
+	err = destination.SetAddress(inflationDestinationPublicKey)
+	if err != nil {
+		panic(err)
+	}
+	clearF := xdr.Uint32(clearFlag)
+	setF := xdr.Uint32(setFlag)
+	masterW := xdr.Uint32(masterWeight)
+	lowT := xdr.Uint32(lowThreshold)
+	medT := xdr.Uint32(medThreshold)
+	highT := xdr.Uint32(highThreshold)
+	homeD := xdr.String32(homeDomain)
+	var signer xdr.Signer
+	signer.Weight = xdr.Uint32(singerWeight)
+	err = signer.Key.SetAddress(singerPublicKey)
+	if err != nil {
+		panic(err)
+	}
+
+	op := xdr.SetOptionsOp{
+		InflationDest: &destination,
+		ClearFlags:    &clearF,
+		SetFlags:      &setF,
+		MasterWeight:  &masterW,
+		LowThreshold:  &lowT,
+		MedThreshold:  &medT,
+		HighThreshold: &highT,
+		HomeDomain:    &homeD,
+		Signer:        &signer,
+	}
+	respXDR, err = xdr.MarshalBase64(op)
+
+	return respXDR, err
+}
+
+//Not found yet
+// func AccountMerge() {
+// }
---
+++ xdr_builder_test.go
@@ -0,0 +1,153 @@
+package xdrbuilder_test
+
+import (
+	xdrHelper "stellar-xdr-helper"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetAsset(t *testing.T) {
+	code := "ABC"
+	issuerPublicKey := "GAEBJVQJJO5ZPRJ2ZPNSDJLMNN64REZO7S5VUZAMNLI34B5XUQVD3URR"
+	resp, err := xdrHelper.SetAsset(code, issuerPublicKey)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestSetNativeAsset(t *testing.T) {
+	resp, err := xdrHelper.SetNativeAsset()
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestCreateAccount(t *testing.T) {
+	destinationPublicKey := "GDKV36XRERL7HVQ5GKRAV47ZLEPIZMFM7MMLEO4NKQOWFPL5NCIEW3GR"
+	startingBalance := 100
+	resp, err := xdrHelper.CreateAccount(destinationPublicKey, startingBalance)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestPayment(t *testing.T) {
+	destinationPublicKey := "GDKV36XRERL7HVQ5GKRAV47ZLEPIZMFM7MMLEO4NKQOWFPL5NCIEW3GR"
+	asset, err := xdrHelper.SetAsset("ABC", "GAEBJVQJJO5ZPRJ2ZPNSDJLMNN64REZO7S5VUZAMNLI34B5XUQVD3URR")
+	if err != nil {
+		panic(err)
+	}
+	amount := 100
+	resp, err := xdrHelper.Payment(destinationPublicKey, asset, amount)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestManageOffer(t *testing.T) {
+	sellingAsset, err := xdrHelper.SetAsset("ABC", "GAEBJVQJJO5ZPRJ2ZPNSDJLMNN64REZO7S5VUZAMNLI34B5XUQVD3URR")
+	if err != nil {
+		panic(err)
+	}
+	buyingAsset, err := xdrHelper.SetNativeAsset()
+	if err != nil {
+		panic(err)
+	}
+	amount := 1000
+	pricestring := "7.2"
+	resp, err := xdrHelper.ManageOffer(sellingAsset, buyingAsset, amount, pricestring)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestCreatePassiveOffer(t *testing.T) {
+	sellingAsset, err := xdrHelper.SetAsset("ABC", "GAEBJVQJJO5ZPRJ2ZPNSDJLMNN64REZO7S5VUZAMNLI34B5XUQVD3URR")
+	if err != nil {
+		panic(err)
+	}
+	buyingAsset, err := xdrHelper.SetNativeAsset()
+	if err != nil {
+		panic(err)
+	}
+	amount := 10000
+	pricestring := "6.2"
+	resp, err := xdrHelper.CreatePassiveOffer(sellingAsset, buyingAsset, amount, pricestring)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestAllowTrust(t *testing.T) {
+	trustorPublicKey := "GDKV36XRERL7HVQ5GKRAV47ZLEPIZMFM7MMLEO4NKQOWFPL5NCIEW3GR"
+	asset, err := xdrHelper.SetAsset("ABC", "GAEBJVQJJO5ZPRJ2ZPNSDJLMNN64REZO7S5VUZAMNLI34B5XUQVD3URR")
+	if err != nil {
+		panic(err)
+	}
+	authorize := true
+	resp, err := xdrHelper.AllowTrust(trustorPublicKey, asset, authorize)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestPathPayment(t *testing.T) {
+	sendAsset, err := xdrHelper.SetAsset("ABC", "GAEBJVQJJO5ZPRJ2ZPNSDJLMNN64REZO7S5VUZAMNLI34B5XUQVD3URR")
+	if err != nil {
+		panic(err)
+	}
+	sendMax := 100
+	destinationPublicKey := "GCIQJ3JRXEEAKFL22C43X66B4NKACPWZ27WIMNXGA5CIEHOYWNXD3EQR"
+	destAsset, err := xdrHelper.SetAsset("CDF", "GAEBJVQJJO5ZPRJ2ZPNSDJLMNN64REZO7S5VUZAMNLI34B5XUQVD3URR")
+	if err != nil {
+		panic(err)
+	}
+	destAmount := 30
+	tempAsset1, err := xdrHelper.SetNativeAsset()
+	if err != nil {
+		panic(err)
+	}
+	var path xdrHelper.Path
+	path.XDRAsset = append(path.XDRAsset, tempAsset1.XDRAsset)
+
+	resp, err := xdrHelper.PathPayment(sendAsset, sendMax, destinationPublicKey, destAsset, destAmount, path)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestChangeTrust(t *testing.T) {
+	asset, err := xdrHelper.SetAsset("ABC", "GAEBJVQJJO5ZPRJ2ZPNSDJLMNN64REZO7S5VUZAMNLI34B5XUQVD3URR")
+	if err != nil {
+		panic(err)
+	}
+	limit := 1000
+	resp, err := xdrHelper.ChangeTrust(asset, limit)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestBumpSequence(t *testing.T) {
+	sequenceNumber := 1008097543847999
+	resp, err := xdrHelper.BumpSequence(sequenceNumber)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestManageData(t *testing.T) {
+	name := "name"
+	value := "value"
+	resp, err := xdrHelper.ManageData(name, value)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestSetOption(t *testing.T) {
+	inflationDestinationPublicKey := "GCIQJ3JRXEEAKFL22C43X66B4NKACPWZ27WIMNXGA5CIEHOYWNXD3EQR"
+	clearFlag := uint32(1)
+	setFlag := uint32(2)
+	masterWeight := uint32(7)
+	lowThreshold := uint32(1)
+	medThreshold := uint32(2)
+	highThreshold := uint32(3)
+	homeDomain := "r0ix.com"
+	singerWeight := uint32(3)
+	singerPublicKey := "GCWQICM6L52IERBVY3UHZVINP6VB3W52RHDKQGGNRNSXPXK4RB3SFAWE"
+	resp, err := xdrHelper.SetOption(inflationDestinationPublicKey, clearFlag, setFlag, masterWeight, lowThreshold,
+		medThreshold, highThreshold, homeDomain, singerPublicKey, singerWeight)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
